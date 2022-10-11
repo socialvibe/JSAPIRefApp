90424d6be77a8ae3955687bcfef1acefa991adc9
Yaroslav Dukal <yaroslav.dukal@infillion.com>
2022-10-10 19:28:20 -0500

    - updated minSDK to 23 - added JS variables to Kotlin - added Logging from JS

--- android/app/src/main/java/com/jsapirefapp/app/WebViewActivity.kt
+++ android/app/src/main/java/com/jsapirefapp/app/WebViewActivity.kt
@@ -3,12 +3,16 @@ package com.jsapirefapp.app
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.util.Log
+import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 
 
 class WebViewActivity : AppCompatActivity() {
+
+    private val clientId: String = "SET_ME"
+    private val partnerConfigHash: String = "SET_ME"
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,7 @@ class WebViewActivity : AppCompatActivity() {
 
         myWebView.settings.javaScriptEnabled = true
         myWebView.settings.allowContentAccess = true
+        myWebView.addJavascriptInterface(this, "Android")
 
         myWebView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
@@ -30,6 +35,25 @@ class WebViewActivity : AppCompatActivity() {
             }
         }
 
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                Log.d("WebView", "${consoleMessage.message()} -- From line " +
+                        "${consoleMessage.lineNumber()} of ${consoleMessage.sourceId()}")
+                return true
+            }
+        }
+
         myWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    /** Use this functions for JS script  */
+    @JavascriptInterface
+    fun getClientID(): String {
+        return clientId
+    }
+
+    @JavascriptInterface
+    fun getClientHash(): String {
+        return partnerConfigHash
     }
 }
